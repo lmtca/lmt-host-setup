1ebf5f75cb60109b2294244b04d1402a830d6fdc
Jordan Trask <jtrask@gmail.com>
2025-03-06 18:39:52 -0500

    Release 0.1.2
    Refactored Directory Structure
    Added csf-webhook
    Added configuration file.

--- csf-webhook/csf-webhook.sh
+++ csf-webhook/csf-webhook.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # -- Variables
-EMAIL_CONTENT="$(cat)"
 OUTPUT=""
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATE_START="$(date)"
@@ -10,7 +9,11 @@ LOG_FILE_SIZE=50MB # 10MB
 LOG_ROTATE=5
 LOG_FILE="$SCRIPT_DIR/csf-webhook.log"
 DEBUG="0"
-WEBHOOK_URL=""
+
+# -- Source the configuration file
+if [ -f "$SCRIPT_DIR/csf-webhook.conf" ]; then
+    source "$SCRIPT_DIR/csf-webhook.conf"
+fi
 
 # ==============================================================================
 # -- Functions
@@ -18,8 +21,13 @@ WEBHOOK_URL=""
 # =====================================
 # -- Debug function
 # =====================================
-function debug () {
-    [[ "$DEBUG" == "1" ]] && { OUTPUT+="$@\n";}
+function _debug () {
+    [[ "$DEBUG" == "1" ]] && { OUTPUT+="${*}\n";}
+}
+
+function _log () {
+    # Add to $OUTPUT
+    OUTPUT+="${*}\n"
 }
 
 # =====================================
@@ -55,35 +63,118 @@ check_log_size () {
     fi
 }
 
-debug "==================================================================="
-debug "Started at $DATE_START"
+# =====================================
+# -- process_email
+# =====================================
+process_email () {
+    EMAIL="${*}"
+    # Extract sender
+    CSF_SENDER=$(echo "$EMAIL" | grep -oP '(?<=From: ).*')
+    # Extract subject
+    CSF_SUBJECT=$(echo "$EMAIL" | grep -oP '(?<=Subject: ).*')
+}
+
+# ==============================================================================
+# -- Arguments
+# ==============================================================================
+while getopts ":dtp" opt; do
+    case $opt in
+        p)
+            MODE="test-data"        
+            ;;
+        t)
+            MODE="test"
+            ;;
+        d)
+            DEBUG="1"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+
+# ==============================================================================
+# -- Main
+# ==============================================================================
+
+# -- Check if WEBHOOK_URL is set
+if [ -z "$WEBHOOK_URL" ]; then
+    echo "WEBHOOK_URL is not set. Please set it in the configuration file."
+    exit 1
+fi
+
+# -- Check log size
+check_log_size
+
+# -- Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install it."
+    exit 1
+fi
+
+# -- Check if test mode is enabled
+if [ "$MODE" == "test" ]; then
+    if [ ! -f "$SCRIPT_DIR/test-email.txt" ]; then
+        echo "Test email file not found. Please create a $SCRIPT_DIR/test-email.txt file in the same directory."
+        exit 1
+    fi
+    CSF_CONTENT=$(cat $SCRIPT_DIR/test-email.txt)
+    echo "Test mode enabled."
+    echo "Test email content:"
+    echo "==================================================="
+    echo "$CSF_CONTENT"
+    echo "==================================================="
+    process_email "$CSF_CONTENT"
+    echo "Sender: $CSF_SENDER"
+    echo "Subject: $CSF_SUBJECT"
+    echo "Body: $CSF_CONTENT"
+    exit 0
+elif [ "$MODE" == "test-data" ]; then
+    if [ ! -f "$SCRIPT_DIR/test-email.txt" ]; then
+        echo "Test email file not found. Please create a $SCRIPT_DIR/test-email.txt file in the same directory."
+        exit 1
+    fi
+    CSF_CONTENT=$(cat $SCRIPT_DIR/test-email.txt)
+    process_email "$CSF_CONTENT"
+else
+    CSF_CONTENT=$(cat)
+    process_email "$CSF_CONTENT"
+fi
+
+_log "==================================================================="
+_log "Started at $DATE_START"
 # Read the email from standard input (where Postfix will pipe it)
 
-debug "Sending data to $WEBHOOK_URL"
-debug "$CSF_CONTENT"
-
-# Extract relevant information from the email (customize as needed)
-sender=$(echo "$email" | grep -oP 'From: \K.*')
-subject=$(echo "$email" | grep -oP 'Subject: \K.*')
-body=$(echo "$email" | sed '1,/^$/d')  # Extract body after headers
+_log "Sending data to $WEBHOOK_URL"
+_log "---------------------------------------------------"
+_log "Sender: $CSF_SENDER"
+_log "---------------------------------------------------"
+_log "Subject: $CSF_SUBJECT"
+_log "---------------------------------------------------"
+_log "Body: $CSF_CONTENT"
+_log "---------------------------------------------------"
 
 # Prepare the data for the webhook (e.g., as JSON)
-payload=$(jq -n \
-          --arg sender "$sender" \
-          --arg subject "$subject" \
-          --arg body "$body" \
+PAYLOAD=$(jq -n \
+          --arg sender "$CSF_SENDER" \
+          --arg subject "$CSF_SUBJECT" \
+          --arg body "$CSF_CONTENT" \
           '{sender: $sender, subject: $subject, body: $body}')
 
-debug "Payload: $payload"
+_log "Payload: $PAYLOAD"
 
 # Send the data to your webhook
 CURL_OUTPUT=$(curl -s -X POST \
      -H "Content-Type: application/json" \
-     -d "$payload" \
+     -d "$PAYLOAD" \
      "$WEBHOOK_URL")
 
-debug "$CURL_OUTPUT"
+_log "$CURL_OUTPUT"
 DATE_END="$(date)"
-debug "Finished at $DATE_END"
-debug "==================================================================="
-echo -e "$OUTPUT" >> $LOG_FILE
+_log "Finished at $DATE_END"
+_log "==================================================================="
+
+echo -e "$OUTPUT" >> "$LOG_FILE"
+[[ $DEBUG == "1" ]] && echo -e "$OUTPUT"
